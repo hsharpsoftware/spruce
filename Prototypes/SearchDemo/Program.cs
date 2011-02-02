f30f9727e5a9e31565d22a93aec56e1c41ec06c7
Chris Small <mrshrinkray@gmail.com>
2011-02-02 07:52:42 +0000

    missing addremove from last changeset

---
+++ Prototypes/SearchDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using com.calitha.goldparser;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string compiledFile = "spruce.cgt";
+			string searchTerm = "-cheddar OR -brie \"red leicester\" OR -\"chutney\" project:foo";
+			
+			Console.WriteLine("[Searching for:]");
+			Console.WriteLine(searchTerm);
+			Console.WriteLine();
+
+			using (FileStream fileStream = new FileStream(compiledFile, FileMode.Open))
+			{
+				// Parse the compiled grammar file
+				CGTReader reader = new CGTReader(fileStream);
+				LALRParser lalrParser = reader.CreateNewParser();
+
+				// Register for events
+				lalrParser.OnTokenRead += new LALRParser.TokenReadHandler(lalrParser_OnTokenRead);
+				lalrParser.OnReduce += new LALRParser.ReduceHandler(lalrParser_OnReduce);
+				lalrParser.OnShift += new LALRParser.ShiftHandler(lalrParser_OnShift);
+				lalrParser.Parse(searchTerm);
+			}
+
+			Console.Read();
+		}
+
+		static void lalrParser_OnShift(LALRParser parser, ShiftEventArgs args)
+		{
+			
+		}
+
+		static void lalrParser_OnReduce(LALRParser parser, ReduceEventArgs args)
+		{
+			
+		}
+
+		static void lalrParser_OnTokenRead(LALRParser parser, TokenReadEventArgs args)
+		{
+			Console.WriteLine("{0,-30}:\t{2}",args.Token.Symbol,"",args.Token.Text);
+		}
+	}
+}
