8aa9005f7f4d5d713ad0dd0256e12075130065bb
Chris Small <mrshrinkray@gmail.com>
2011-02-07 23:23:45 +0000

    First aspects of the searching for the core

---
+++ Spruce.Core/Search/SearchParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.calitha.goldparser;
+using System.IO;
+
+namespace Spruce.Core.Search
+{
+	public class SearchParser
+	{
+		// Passes a WIQL string
+		public event EventHandler<EventArgs> ParseComplete;
+
+		public static CGTReader GrammarReader
+		{
+			get
+			{
+				if (_grammarReader == null)
+					InitializeReader();
+
+				return _grammarReader;
+			}
+		}
+
+		private static CGTReader _grammarReader;
+		private WiqlBuilder _wiqlBuilder;
+
+		public static void InitializeReader()
+		{
+			using (Stream stream = typeof(SearchParser).Assembly.GetManifestResourceStream("Spruce.Core.Search.Spruce.cgt"))
+			{
+				// Parse the compiled grammar file
+				_grammarReader = new CGTReader(stream);
+			}
+		}
+
+		public void SearchFor(string searchTerm)
+		{
+			LALRParser lalrParser = GrammarReader.CreateNewParser();
+			_wiqlBuilder = new WiqlBuilder();
+
+			// Register for events
+			lalrParser.OnTokenRead += new LALRParser.TokenReadHandler(lalrParser_OnTokenRead);
+			lalrParser.Parse(searchTerm);
+		}
+
+		private void lalrParser_OnTokenRead(LALRParser parser, TokenReadEventArgs args)
+		{
+			// Huge switch statement
+
+			// Pop a Term onto the queue
+
+			// Peek top queue item
+
+			// 
+		}
+	}
+}
---
+++ Spruce.Core/Search/WiqlBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spruce.Core.Search
+{
+	public class WiqlBuilder
+	{
+		private StringBuilder _builder;
+
+		public WiqlBuilder()
+		{
+			_builder = new StringBuilder();
+		}
+
+		public WiqlBuilder And()
+		{
+			return this;
+		}
+
+		public WiqlBuilder Or()
+		{
+			return this;
+		}
+
+		public WiqlBuilder FieldEquals(string field,string value)
+		{
+			return this;
+		}
+	}
+}
