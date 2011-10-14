f61a802a63f3948503593bcbb8ad2cea4b85b680
Chris Small <mrshrinkray@gmail.com>
2011-10-14 23:09:34 +0100

    XML Documentation and refactors completed + fixes for views

---
+++ Spruce.Core/Domain/User/FilterValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spruce.Core
+{
+	/// <summary>
+	/// The values for the table column filters on list data.
+	/// </summary>
+	public class ListFilterValues
+	{
+		public string Title { get; set; }
+		public string AssignedTo { get; set; }
+		public string StartDate { get; set; }
+		public string EndDate { get; set; }
+		public string Status { get; set; }
+		public bool IsDescending { get; set; }
+		public string SortBy { get; set; }
+
+		public int CurrentPage { get; set; }
+		public int PageCount { get; set; }
+		public int PageSize { get; set; }
+
+		public ListFilterValues()
+		{
+			PageSize = 1;
+			CurrentPage = 1;
+		}
+	}
+}
--- Spruce.Templates.MSAgile/Common/Extensions.cs
+++ Spruce.Templates.MSAgile/Common/Extensions.cs
@@ -12,31 +12,43 @@ using System.Text.RegularExpressions;
 
 namespace Spruce.Templates.MSAgile
 {
-	public static class HtmlExtensions
+	/// <summary>
+	/// Core extensions for the MGAgile template.
+	/// </summary>
+	public static class Extensions
 	{
+		/// <summary>
+		/// Produces a CSV represenation of the provided <see cref="BugSummary"/>.
+		/// </summary>
 		public static string ToCsv(this BugSummary summary)
 		{
 			StringBuilder builder = new StringBuilder();
-			builder.Append(ToCsv(summary));
+			builder.Append(((WorkItemSummary)summary).ToCsv());
 
 			// Priority
 			builder.Append(summary.Priority);
 			builder.Append(",");
 
 			// Severity
-			if (summary.Severity.IndexOf(",") > -1)
-				builder.Append("\"" + summary.Severity + "\"");
-			else
-				builder.Append(summary.Title);
+			string severity = summary.Severity.Replace("\"", "\"\"");
 
+			if (severity.IndexOf(",") > -1)
+				builder.Append("\"" + severity + "\"");
+			else
+				builder.Append(severity);
+
+			builder.Append(",");
 
 			return builder.AppendLine().ToString();
 		}
 
+		/// <summary>
+		/// Produces a CSV represenation of the provided <see cref="TaskSummary"/>.
+		/// </summary>
 		public static string ToCsv(this TaskSummary summary)
 		{
 			StringBuilder builder = new StringBuilder();
-			builder.Append(ToCsv(summary));
+			builder.Append(((WorkItemSummary)summary).ToCsv());
 
 			// Priority
 			builder.Append(summary.Priority);
